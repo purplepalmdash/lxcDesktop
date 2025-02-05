72c3996182484b97de4e4548c137474900bb75b0
purplepalmdash <purplepalmdash@users.noreply.github.com>
2025-02-05 08:48:17 +0000

    deploy: 2d96fa94985b0b2aa45c5b4ed0fa7cd40d17676d

--- toc.js
+++ toc.js
@@ -8,7 +8,7 @@ class MDBookSidebarScrollbox extends HTMLElement {
         super();
     }
     connectedCallback() {
-        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded "><a href="x86debianhost.html"><strong aria-hidden="true">1.</strong> x86_debian_host_setting</a></li><li class="chapter-item expanded "><a href="3a6000.html"><strong aria-hidden="true">2.</strong> Loongson 3A6000</a></li></ol>';
+        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded "><a href="x86debianhost.html"><strong aria-hidden="true">1.</strong> x86_debian_host_setting</a></li><li class="chapter-item expanded "><a href="x86debianvirshlxc.html"><strong aria-hidden="true">2.</strong> x86_debian_virsh_lxc</a></li><li class="chapter-item expanded "><a href="3a6000.html"><strong aria-hidden="true">3.</strong> Loongson 3A6000</a></li></ol>';
         // Set the current, active page, and reveal it if it's hidden
         let current_page = document.location.href.toString().split("#")[0];
         if (current_page.endsWith("/")) {
